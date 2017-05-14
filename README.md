3fe21ace8ddb5da83ae8c0bc8ec730fbdc3a7527
Bi Banh Bao <skisssbb@hotmail.de>
2017-05-14 02:47:17 +0200

    Dummy Jersey Server
    Dummy Jearsey Server is running on Grizzly 2 based on HTTP Protocol. To start run following commands:
    1) mvn clean compile
    2) mvn exec:java
    To create a stand alone jar file for deploy, run following command:
    1) mvn clean compile
    2) mvn package

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 qd18jemy-bp17-jerseyserver
+
+Dummy Jearsey Server is running on Grizzly 2 based on HTTP Protocol.
+Requirement is that you have installed Maven on your system.
+
+To start, run following commands:
+1) mvn clean compile
+2) mvn exec:java
+3) The Server can be reached on browser under: http://localhost:8080/bp17.jerseyserver/myresource
+
+To create a stand alone jar file for deploying, run following command:
+1) mvn clean compile
+2) mvn package
+3) The jar file can be found under target dir
---
+++ src/main/java/bp17/jerseyserver/MyResource.java
@@ -0,0 +1,27 @@
+package bp17.jerseyserver;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ * Service accessible under
+ * http://localhost:8080/bp17.jerseyserver/myresource
+ */
+@Path("myresource")
+public class MyResource {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "HTTP GET Request Successful!";
+    }
+}
