f8ccf157e03f990ca1fd255bf4c68da4e66fd9a8
Vincent Riesop <Vincent.Riesop@gmail.com>
2017-05-18 10:32:10 +0200

    Hibernate Support Added
    Hibernate + Postgresql Support added
    Added Demo Service to save Demo Models to the postgresql DB

---
+++ src/main/java/bp17/jerseyserver/BarriersService.java
@@ -0,0 +1,53 @@
+package bp17.jerseyserver;
+
+import bp17.model.Barrier;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by Vincent on 18.05.2017.
+ */
+@Path("/barriers")
+public class BarriersService {
+
+
+    @POST
+    @Path("/post")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createTrackInJSON(Barrier barrier) {
+        String result = "Barrier Saved: " + barrier;
+
+        try{
+            // Save the Barrier via Hibernate to the Database.
+            Configuration config = new Configuration();
+            config.configure();
+            SessionFactory sessionFactory = config.buildSessionFactory();
+
+            Session session = sessionFactory.openSession();
+            session.beginTransaction();
+            session.save(barrier);
+            session.getTransaction().commit();
+
+        } catch (Exception e){
+            System.out.println(e.toString());
+
+        }
+
+        return Response.status(201).entity(result).build();
+    }
+
+
+    @GET
+    @Path("/{id}")
+    public Response getIt(@PathParam("id") String id) {
+
+        return Response.status(200).entity("Barrier with, id : " + id).build();
+    }
+
+}
--- src/main/java/bp17/model/Barrier.java
+++ src/main/java/bp17/model/Barrier.java
@@ -1,13 +1,22 @@
 package bp17.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  * Created by Vincent on 16.05.2017.
  */
+@Entity
 public class Barrier {
+
+    @Id
+    private int barrierId;
 
     private String name;
 
     private double longitude = 49.874978;
+
     private double latitude = 8.655971;
 
     public Barrier(){
@@ -37,5 +46,13 @@ public class Barrier {
 
     public void setLongitude(double longitude) {
         this.longitude = longitude;
+    }
+
+    public int getBarrierId() {
+        return barrierId;
+    }
+
+    public void setBarrierId(int barrierId) {
+        this.barrierId = barrierId;
     }
 }
