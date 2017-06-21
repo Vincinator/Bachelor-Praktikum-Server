0c85b51bd816c6b8f4de6ef380a8ed031b12a354
Vincent Riesop <Vincent.Riesop@gmail.com>
2017-06-21 17:11:31 +0200

    Obstacles können Hinzugefügt und ausgelesen werden

--- src/main/java/bp17/jerseyserver/BarriersService.java
+++ src/main/java/bp17/jerseyserver/BarriersService.java
@@ -6,10 +6,18 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import javax.management.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import javax.print.attribute.standard.Media;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.util.List;
 
 /**
  * Created by Vincent on 18.05.2017.
@@ -17,6 +25,46 @@ import javax.ws.rs.core.Response;
 @Path("/barriers")
 public class BarriersService {
 
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getObstacles(){
+
+        String result = "";
+
+        try{
+            // Save the Barrier via Hibernate to the Database.
+            Configuration config = new Configuration();
+            config.configure("hibernate.cfg.xml"); //populates the data of the configuration file
+            SessionFactory sessionFactory = config.buildSessionFactory();
+
+            Session session = sessionFactory.openSession();
+
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<Obstacle> criteria = builder.createQuery(Obstacle.class);
+            Root<Obstacle> obstacleRoot = criteria.from(Obstacle.class);
+
+            criteria.select(obstacleRoot);
+
+            List<Obstacle> obstacles = session.createQuery(criteria).getResultList();
+
+            session.close();
+
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            result = mapper.writeValueAsString(obstacles);
+
+
+        } catch (Exception e){
+            // TODO: Entferne Exception aus Response für Produktiveinsatz.
+            // Für Debugging wird die Exception als Response ausgegeben.
+            return Response.status(503).entity(e.toString()).build();
+        }
+
+        return Response.status(200).entity(result).build();
+
+    }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
