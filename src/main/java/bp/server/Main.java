b74c3ff3636f44ef148d95fbd40ed50097af6f92
Vincent Riesop <Vincent.Riesop@datron.de>
2017-07-03 19:02:29 +0200

    checkstyle

--- src/main/java/bp/server/Main.java
+++ src/main/java/bp/server/Main.java
@@ -1,41 +1,30 @@
 package bp.server;
 
+import java.io.IOException;
+import java.net.URI;
+
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
-import java.io.IOException;
-import java.net.URI;
-
 /**
  * Main class.
- *
  */
 public class Main {
 
-    public static final String BASE_URI = "http://127.0.0.1:8082/routing/";
+  private static final String BASE_URI = "http://127.0.0.1:8082/routing/";
 
-    /**
-     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-     * @return Grizzly HTTP server.
-     */
-    public static HttpServer startServer() {
-        // create a resource config that scans for JAX-RS resources and providers
-        final ResourceConfig rc = new ResourceConfig().packages("bp.server");
 
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-    }
+  public static HttpServer startServer() {
+    final ResourceConfig rc = new ResourceConfig().packages("bp.server");
+    return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+  }
 
-    /**
-     * Main method.
-     * @param args
-     * @throws IOException
-     */
-    public static void main(String[] args) throws IOException {
-        final HttpServer server = startServer();
-
-    }
+  /**
+   * Main method.
+   */
+  public static void main(String[] args) throws IOException {
+    final HttpServer server = startServer();
+  }
 }
 
--- src/main/java/bp/server/service/BarriersService.java
+++ src/main/java/bp/server/service/BarriersService.java
@@ -1,20 +1,30 @@
 package bp.server.service;
 
-import bp.common.model.*;
+import bp.common.model.Construction;
+import bp.common.model.Elevator;
+import bp.common.model.FastTrafficLight;
+import bp.common.model.Ramp;
+import bp.common.model.Stairs;
+import bp.common.model.TightPassage;
+import bp.common.model.Unevenness;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
+import java.util.List;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 
 /**
@@ -23,166 +33,213 @@ import java.util.List;
 @Path("/barriers")
 public class BarriersService {
 
-    private static Session getSession() {
-        // Save the Barrier via Hibernate to the Database.
-        Configuration config = new Configuration();
-        config.configure("hibernate.cfg.xml"); //populates the data of the configuration file
-        SessionFactory sessionFactory = config.buildSessionFactory();
-        return sessionFactory.openSession();
+  /** Starts a new Hibernate session.
+   * @return a new session
+   */
+  private static Session getSession() {
+    // Save the Barrier via Hibernate to the Database.
+    Configuration config = new Configuration();
+    config.configure("hibernate.cfg.xml");
+    SessionFactory sessionFactory = config.buildSessionFactory();
+    return sessionFactory.openSession();
+  }
+
+  /** Returns all barriers as JSON with the specified type from the database.
+   * @param typeParameterClass variable for the type of barrier
+   * @param <T> Type of Barrier
+   * @return a JSON list with all Barriers of the specified type
+   * @throws JsonProcessingException if Jackson throws exception
+   */
+  public static <T> String getData(Class<T> typeParameterClass)
+      throws JsonProcessingException {
+
+    // Save the Barrier via Hibernate to the Database.
+    Configuration config = new Configuration();
+    config.configure("hibernate.cfg.xml");
+    SessionFactory sessionFactory = config.buildSessionFactory();
+    Session session = sessionFactory.openSession();
+    CriteriaBuilder builder = session.getCriteriaBuilder();
+    CriteriaQuery<T> criteria = builder.createQuery(typeParameterClass);
+    Root<T> root = criteria.from(typeParameterClass);
+    criteria.select(root);
+    List<T> datalist = session.createQuery(criteria).getResultList();
+    session.close();
+
+    ObjectMapper mapper = new ObjectMapper();
+    JavaType listJavaType =
+        mapper.getTypeFactory().constructCollectionType(List.class, typeParameterClass);
+
+    return mapper.writerWithType(listJavaType).writeValueAsString(datalist);
+  }
+
+  /** (Jersey) API for retrieving all stairs as JSON List.
+   * @return Stairs as JSON List
+   */
+  @GET
+  @Path("/stairs")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getStairs() {
+
+    String result = "";
+    try {
+      result = getData(Stairs.class);
+
+    } catch (Exception e) {
+      return Response.status(503).entity(e.toString()).build();
     }
+    return Response.status(200).entity(result).build();
 
-    public static <T> String getData (Class<T> typeParameterClass) throws JsonProcessingException {
+  }
 
-        // Save the Barrier via Hibernate to the Database.
-        Configuration config = new Configuration();
-        config.configure("hibernate.cfg.xml"); //populates the data of the configuration file
-        SessionFactory sessionFactory = config.buildSessionFactory();
-        Session session = sessionFactory.openSession();
-        CriteriaBuilder builder = session.getCriteriaBuilder();
-        CriteriaQuery<T> criteria = builder.createQuery(typeParameterClass);
-        Root<T> root = criteria.from(typeParameterClass);
-        criteria.select(root);
-        List<T> datalist = session.createQuery(criteria).getResultList();
-        session.close();
 
-        ObjectMapper mapper = new ObjectMapper();
-        JavaType listJavaType = mapper.getTypeFactory().constructCollectionType(List.class, typeParameterClass);
+  /** (Jersey) API for retrieving all ramps as JSON List.
+   * @return Ramps as JSON List
+   */
+  @GET
+  @Path("/ramps")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getRamps() {
+    String result = "";
+    try {
+      result = getData(Ramp.class);
 
-        return mapper.writerWithType(listJavaType).writeValueAsString(datalist);
+    } catch (Exception e) {
+      return Response.status(503).entity(e.toString()).build();
     }
+    return Response.status(200).entity(result).build();
+  }
 
-    @GET
-    @Path("/stairs")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getStairs(){
-
-        String result = "";
-        try{
-            result = getData(Stairs.class);
-
-        } catch (Exception e){
-            return Response.status(503).entity(e.toString()).build();
-        }
-        return Response.status(200).entity(result).build();
-
+  /** (Jersey) API for retrieving all unevennesses as JSON List.
+   * @return unevennesses as JSON List
+   */
+  @GET
+  @Path("/unevennesses")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getUnevennesses() {
+    String result = "";
+    try {
+      result = getData(Unevenness.class);
+    } catch (Exception e) {
+      return Response.status(503).entity(e.toString()).build();
     }
+    return Response.status(200).entity(result).build();
+  }
 
-    @GET
-    @Path("/ramps")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getRamps(){
-        String result = "";
-        try{
-            result = getData(Ramp.class);
 
-        } catch (Exception e){
-            return Response.status(503).entity(e.toString()).build();
-        }
-        return Response.status(200).entity(result).build();
+  /** (Jersey) API for retrieving all tightpassages as JSON List.
+   * @return tightpassages as JSON List
+   */
+  @GET
+  @Path("/tightpassages")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getTightPassages() {
+    String result = "";
+    try {
+      result = getData(TightPassage.class);
+    } catch (Exception e) {
+      return Response.status(503).entity(e.toString()).build();
     }
+    return Response.status(200).entity(result).build();
+  }
 
-    @GET
-    @Path("/unevennesses")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getUnevennesses(){
-        String result = "";
-        try{
-            result = getData(Unevenness.class);
-        } catch (Exception e){
-            return Response.status(503).entity(e.toString()).build();
-        }
-        return Response.status(200).entity(result).build();
+
+  /** (Jersey) API for retrieving all elevators as JSON List.
+   * @return elevators as JSON List
+   */
+  @GET
+  @Path("/elevators")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getElevators() {
+    String result = "";
+    try {
+      result = getData(Elevator.class);
+    } catch (Exception e) {
+      return Response.status(503).entity(e.toString()).build();
     }
+    return Response.status(200).entity(result).build();
+  }
 
-    @GET
-    @Path("/tightpassages")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getTightPassages(){
-        String result = "";
-        try{
-            result = getData(TightPassage.class);
-        } catch (Exception e){
-            return Response.status(503).entity(e.toString()).build();
-        }
-        return Response.status(200).entity(result).build();
+
+  /** (Jersey) API for retrieving all fasttrafficlights as JSON List.
+   * @return fasttrafficlights as JSON List
+   */
+  @GET
+  @Path("/fasttrafficlights")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getFastTrafficLights() {
+    String result = "";
+    try {
+      result = getData(FastTrafficLight.class);
+    } catch (Exception e) {
+      return Response.status(503).entity(e.toString()).build();
     }
+    return Response.status(200).entity(result).build();
+  }
 
-    @GET
-    @Path("/elevators")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getElevators(){
-        String result = "";
-        try{
-            result = getData(Elevator.class);
-        } catch (Exception e){
-            return Response.status(503).entity(e.toString()).build();
-        }
-        return Response.status(200).entity(result).build();
+
+  /** (Jersey) API for retrieving all constructions as JSON List.
+   * @return constructions as JSON List
+   */
+  @GET
+  @Path("/constructions")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getConstructions() {
+    String result = "";
+    try {
+      result = getData(Construction.class);
+
+    } catch (Exception e) {
+      return Response.status(503).entity(e.toString()).build();
     }
+    return Response.status(200).entity(result).build();
+  }
 
-    @GET
-    @Path("/fasttrafficlights")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getFastTrafficLights(){
-        String result = "";
-        try{
-            result = getData(FastTrafficLight.class);
-        } catch (Exception e){
-            return Response.status(503).entity(e.toString()).build();
-        }
-        return Response.status(200).entity(result).build();
+  /** (Jersey) API exposes the POST interface.
+   * Creates new stairs
+   * @return HTTP Result
+   */
+  @POST
+  @Path("/stairs")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response postNewObstacle(Stairs stairs) {
+    String result = "Stairs hinzugefügt: " + stairs;
+    try {
+      Session session = getSession();
+      session.beginTransaction();
+      session.save(stairs);
+      session.getTransaction().commit();
+    } catch (Exception e) {
+      return Response.status(503).entity(e.toString()).build();
     }
-    @GET
-    @Path("/constructions")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getConstructions(){
-        String result = "";
-        try{
-            result = getData(Construction.class);
+    return Response.status(201).entity(result).build();
+  }
 
-        } catch (Exception e){
-            return Response.status(503).entity(e.toString()).build();
-        }
-        return Response.status(200).entity(result).build();
+
+  /** (Jersey) API exposes the POST interface.
+   * Creates new ramps
+   * @return HTTP Result
+   */
+  @POST
+  @Path("/ramps")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response postNewObstacle(Ramp ramp) {
+    String result = "Ramp hinzugefügt: " + ramp;
+    try {
+      Session session = getSession();
+      session.beginTransaction();
+      session.save(ramp);
+      session.getTransaction().commit();
+    } catch (Exception e) {
+      return Response.status(503).entity(e.toString()).build();
     }
+    return Response.status(201).entity(result).build();
+  }
 
-    @POST
-    @Path("/stairs")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response postNewObstacle(Stairs stairs) {
-        String result = "Stairs hinzugefügt: " + stairs;
-        try{
-            Session session = getSession();
-            session.beginTransaction();
-            session.save(stairs);
-            session.getTransaction().commit();
-        } catch (Exception e){
-            return Response.status(503).entity(e.toString()).build();
-        }
-        return Response.status(201).entity(result).build();
-    }
+  @GET
+  @Path("/{id}")
+  public Response getIttt(@PathParam("id") String id) {
 
-    @POST
-    @Path("/ramps")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response postNewObstacle(Ramp ramp) {
-        String result = "Ramp hinzugefügt: " + ramp;
-        try{
-            Session session = getSession();
-            session.beginTransaction();
-            session.save(ramp);
-            session.getTransaction().commit();
-        } catch (Exception e){
-            return Response.status(503).entity(e.toString()).build();
-        }
-        return Response.status(201).entity(result).build();
-    }
-
-    @GET
-    @Path("/{id}")
-    public Response getIttt(@PathParam("id") String id) {
-
-        return Response.status(200).entity("Barrier with, id: " + id).build();
-    }
+    return Response.status(200).entity("Barrier with, id: " + id).build();
+  }
 
 }
