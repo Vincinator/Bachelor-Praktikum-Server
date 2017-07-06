ca77bbcbc436cc48eb0de359a1eabc2bf5a99cdb
Vincent Riesop <Vincent.Riesop@gmail.com>
2017-07-06 22:15:09 +0200

    no cache for docker builds

--- install/docker/deployDockerImage.sh
+++ install/docker/deployDockerImage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker rm bpserver || true
-docker build -t bpserver:latest .
+docker build -no-cache=true -t bpserver:latest .
 docker run --name bpserver -p 8082:8080 -d -it bpserver:latest
 docker exec -d bpserver sudo rm -rf /var/lib/tomcat8/webapps/ROOT/
 docker exec -d bpserver service tomcat8 start
