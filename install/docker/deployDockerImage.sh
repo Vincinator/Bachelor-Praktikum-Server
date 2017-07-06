0d1a8e94b713956bfed694b52afd192d4d48b555
Vincent Riesop <Vincent.Riesop@gmail.com>
2017-07-06 22:16:50 +0200

    stop docker container before removing it

--- install/docker/deployDockerImage.sh
+++ install/docker/deployDockerImage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+docker stop bpserver || true
 docker rm bpserver || true
-docker build -no-cache=true -t bpserver:latest .
+docker build -t bpserver:latest .
 docker run --name bpserver -p 8082:8080 -d -it bpserver:latest
 docker exec -d bpserver sudo rm -rf /var/lib/tomcat8/webapps/ROOT/
 docker exec -d bpserver service tomcat8 start
