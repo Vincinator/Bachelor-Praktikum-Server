19ad2d364eb1824751615fc9ee9d1e2f4cd74e29
Vincent Riesop <Vincent.Riesop@gmail.com>
2017-07-06 21:58:19 +0200

    remove bpserver docker container

--- install/docker/deployDockerImage.sh
+++ install/docker/deployDockerImage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker container stop bpserver || true
+docker rm bpserver || true
 docker build -t bpserver:latest .
 docker run --name bpserver -p 8082:8080 -d -it bpserver:latest
 docker exec -d bpserver service tomcat8 start
