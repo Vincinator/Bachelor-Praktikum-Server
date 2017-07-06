a1389ef8ff5941cf504256f17d548100afa74986
Vincent Riesop <Vincent.Riesop@gmail.com>
2017-07-07 00:20:09 +0200

    fixed hibernate issue

--- install/docker/deployDockerImage.sh
+++ install/docker/deployDockerImage.sh
@@ -3,5 +3,5 @@ docker stop bpserver || true
 docker rm bpserver || true
 docker build -t bpserver:latest .
 docker run --name bpserver -p 8082:8080 -d -it bpserver:latest
-docker exec -d bpserver sudo rm -rf /var/lib/tomcat8/webapps/ROOT/
+docker exec -d bpserver service postgresql start
 docker exec -d bpserver service tomcat8 start
--- install/docker/install.sh
+++ install/docker/install.sh
@@ -4,7 +4,6 @@ echo "Installscript started"
 APP_DB_PASS="password"
 webapps_dir=/var/lib/tomcat8/webapps
 
-echo "installing postgresql"
 #apt-get update
 #apt-get install -y postgresql postgresql-contrib sudo default-jre
 
@@ -12,21 +11,18 @@ echo "installing postgresql"
 #                           Postgresql
 ###############################################################
 service postgresql start
-
 echo "CREATE ROLE hibernateuser LOGIN ENCRYPTED PASSWORD '$APP_DB_PASS';" | sudo -u postgres psql
 su postgres -c "createdb hibernatedb --owner hibernateuser"
 
-service postgresql reload
+#service postgresql reload
 
 ###############################################################
 #                           Tomcat
 ###############################################################
 
 
-sudo rm -rf /var/lib/tomcat8/webapps/ROOT/
-cp /opt/ROOT.war $webapps_dir
-chown tomcat8:tomcat8 $webapps_dir/ROOT.war
 
-#service tomcat8 start
+
+service tomcat8 start
 
 # For Development: the server will be started after everything is setup.
