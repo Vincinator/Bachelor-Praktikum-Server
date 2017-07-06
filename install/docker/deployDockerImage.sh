e266b544030dafef541b865ed195000f631d995b
Vincent Riesop <Vincent.Riesop@gmail.com>
2017-07-06 22:03:32 +0200

    remove root folder with docker exec

--- install/docker/deployDockerImage.sh
+++ install/docker/deployDockerImage.sh
@@ -2,4 +2,5 @@
 docker rm bpserver || true
 docker build -t bpserver:latest .
 docker run --name bpserver -p 8082:8080 -d -it bpserver:latest
+docker exec -d bpserver sudo rm -rf /var/lib/tomcat8/webapps/ROOT/
 docker exec -d bpserver service tomcat8 start
