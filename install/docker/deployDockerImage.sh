8a0bf967797c03f47dc0c10d82023bdc85ef6540
Vincent Riesop <Vincent.Riesop@gmail.com>
2017-08-14 15:29:28 +0200

    debug

--- install/docker/deployDockerImage.sh
+++ install/docker/deployDockerImage.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #docker stop bpserver || true
 #docker rm bpserver || true
+pwd
+ls -la
+
 docker build -t bpserver:latest .
 docker run --name bpserver -p 8082:8080 -d -it bpserver:latest
 docker exec -d bpserver service postgresql start
