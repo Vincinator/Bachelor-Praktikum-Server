eea9d01d00f8692594b35d085c9db39a819cc2f4
Bi Banh Bao <skisssbb@hotmail.de>
2017-08-19 17:33:39 +0200

    Update dockerfile (siehe original commit)

--- install/docker/deployDockerImage.sh
+++ install/docker/deployDockerImage.sh
@@ -3,8 +3,9 @@ docker stop bpserver || true
 docker rm bpserver || true
 pwd
 ls -la
-
+echo 'DEBUG: Start Dockerbuilding'
 docker build -t bpserver:latest .
+echo 'DEBUG: END Dockerbuilding'
 docker run --name bpserver -p 8082:8080 -d -it bpserver:latest
 docker exec -d bpserver service postgresql start
 docker exec -d bpserver service tomcat8 start
