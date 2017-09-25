c7ca470c1b37687f596f602721e57b49b237f15c
Vincent Riesop <Vincent.Riesop@gmail.com>
2017-09-25 22:46:23 +0200

    Update InstallDockerContainer.sh

--- InstallDockerContainer.sh
+++ InstallDockerContainer.sh
@@ -28,7 +28,7 @@ cd ..
 docker build -t bpserver:latest . 
 echo 'DEBUG: End Dockerbuilding'
 
-docker run --name bpserver -p 8082:8080 -d -it bpserver:latest
+docker run --name bpserver -p 80:8080 -d -it bpserver:latest
 docker exec -d bpserver service postgresql start
 docker exec -d bpserver service tomcat8 start
 
