2c5779107ab2d36662191ac3b0edf9048b045127
Vincent Riesop <Vincent.Riesop@gmail.com>
2017-09-25 21:21:30 +0200

    change directory back to install folder..

--- InstallDockerContainer.sh
+++ InstallDockerContainer.sh
@@ -23,6 +23,7 @@ git clone https://github.com/skisssbb/BP-ExportTool/
 cd BP-ExportTool
 mvn clean package
 mv target/ExportTool.jar ../
+cd ..
 
 docker build -t bpserver:latest . 
 echo 'DEBUG: End Dockerbuilding'
