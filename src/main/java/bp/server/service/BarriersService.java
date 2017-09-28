37ebec3253408fb84a46217d5e0c7078f0d882c1
Bi Banh Bao <skisssbb@hotmail.de>
2017-09-28 20:47:25 +0200

    Merged Version, Export Tool wird jedes mal beim POST ausgeführt

--- src/main/java/bp/server/service/BarriersService.java
+++ src/main/java/bp/server/service/BarriersService.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
@@ -20,6 +21,8 @@ import javax.persistence.criteria.Root;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.crypto.Data;
+import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -175,7 +178,7 @@ public class BarriersService {
 
   /**
    * (Jersey) API exposes the POST interface.
-   * Creates new stairs
+   * Creates new stairs and add them to Hibernate and OSM DB
    *
    * @return HTTP Result
    */
@@ -207,6 +210,7 @@ public class BarriersService {
     } catch (Exception e) {
       return Response.status(503).entity(e.toString()).build();
     }
+    ExportTool.getInstance().startExportProcess();
     return Response.status(201).entity(result).build();
   }
 
@@ -243,6 +247,7 @@ public class BarriersService {
     } catch (Exception e) {
       return Response.status(503).entity(e.toString()+way.toString()).build();
     }
+    ExportTool.getInstance().startExportProcess();
     return Response.status(201).entity(result).build();
   }
 
@@ -329,82 +334,14 @@ public class BarriersService {
   }
 
   public static void main(String[] args) {
-    double lat1 = 49.877633;
-    double long1 = 8.649615;
-    double lat2 = 49.877405;
-    double long2 = 8.649679;
-    double lat3 = 49.876802;
-    double long3 = 8.649559;
-    double lat4 = 8.111111;
-    double long4 = 49.666666;
-    Node node1 = new Node(lat1, long1);
-    Node node2 = new Node(lat2, long2);
-    Node node3 = new Node(lat3, long3);
-    Node node4 = new Node(lat1, long2);
-    Node node5 = new Node(lat2, long3);
-    Node node6 = new Node(lat3, long1);
-    Node node7 = new Node(lat4,long4);
-    Node node8 = new Node(long4, lat4);
-
-    ArrayList<Node> nodes = new ArrayList<Node>();
-    nodes.add(node1);
-    nodes.add(node2);
-    nodes.add(node3);
-    Way road1 = new Way("", nodes);
-    for (Node n : nodes) n.setWay(road1);
-
-    ArrayList<Node> nodes2 = new ArrayList<>();
-    nodes2.add(node4);
-    nodes2.add(node5);
-    nodes2.add(node6);
-    Way road2 = new Way("", nodes2);
-    for (Node n : nodes2) n.setWay(road2);
-
-    ArrayList<Node> nodes3 = new ArrayList<>();
-    nodes3.add(node7);
-    nodes3.add(node8);
-    Way road3 = new Way("",nodes3);
-    for (Node n : nodes3) n.setWay(road3);
-
-/*
-    Stairs stair2 = new Stairs();
-    Stairs stair1 = new Stairs("sickness",long1, lat1, long2, lat2, 2, "yes");
     BarriersService bs = new BarriersService();
+
+    Stairs stair1 = new Stairs("Lauteschlage", 8.65908, 49.87768, 8.65935, 49.87773, 10, "yes");
+    stair1.setRamp_wheelchair("yes");
+    stair1.setId_way(150032847);
+    stair1.setId_firstnode(1629692805);
+    stair1.setId_lastnode(2623782435L);
+
     bs.postNewStairs(stair1);
-
-
-    Stairs stairs1 = new Stairs("holy", long1, lat1, 52, 12, true);
-    Construction construction1 = new Construction("nothing", long2, lat2, 100, new java.sql.Date(System.currentTimeMillis()));
-
-
-
-    ObjectMapper mapper = new ObjectMapper();
-    String jsonString = "";
-    try {
-      jsonString = mapper.writeValueAsString(road);
-      System.out.println(jsonString);
-    } catch (JsonProcessingException e) {
-      e.printStackTrace();
-    }
-
-    BarriersService bs = new BarriersService();
-    bs.postNewWay(road3);
-    bs.postNewWay(road1);
-    bs.postNewWay(road2);
-    bs.postNewStairs(stairs1);
-    bs.postNewStairs(construction1);
-
-    List<Way> alist = getDataAsList(Way.class);
-    for (Way w : alist) System.out.println(w.getOsm_id() == 0);
-
-    DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-    List<Obstacle> obslist = getDataAsList(Obstacle.class);
-    for (Obstacle ob : obslist) {
-      System.out.println(ob.toString());
-      if (ob instanceof Construction) {
-        Construction cons = (Construction) ob;
-        System.out.printf(String.valueOf(formatter.format(cons.getValidUntil())));
-      }
-    }*/
   }
 }
